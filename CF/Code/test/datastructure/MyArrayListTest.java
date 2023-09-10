8572336e95925b766a172ae3fe1ec4d64e1ae6dc
Mangjun <myoungjun55@naver.com>
2023-09-10 15:19:10 +0900

    Create CF Code MyArrayList, MyArrayListTest

---
+++ CF/Code/test/datastructure/MyArrayListTest.java
@@ -0,0 +1,34 @@
+package datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayListTest {
+
+    @Test
+    void add() {
+        MyArrayList mal = new MyArrayList();
+
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void testClone() {
+    }
+}
